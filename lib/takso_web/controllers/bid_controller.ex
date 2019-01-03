2d3996d645bf30aab415b67230e106307ae61950
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-03 19:30:01 +0200

    empty bid controller

---
+++ lib/takso_web/controllers/bid_controller.ex
@@ -0,0 +1,17 @@
+defmodule TaksoWeb.BidController do
+  use TaksoWeb, :controller
+
+  def summary(conn, _params) do
+
+
+  end
+
+  def create(conn, %{"item_id" => item_id, "bidding" => user_bid, "user" => user}) do
+
+  end
+
+  def bids(conn, %{"item_id" => item_id}) do
+
+  end
+
+end
