716c758ceccbe6b339a19aff86a81b1705e5a9f7
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-04 01:44:27 +0200

    item seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,9 +10,16 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias Takso.{Repo, Accounts.User}
+alias Takso.{Repo, Accounts.User, Item}
 
 [%{name: "Fred Flintstone", username: "fred", password: "parool"},
  %{name: "Barney Rubble", username: "barney", password: "parool"}]
 |> Enum.map(fn user_data -> User.changeset(%User{}, user_data) end)
 |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
+
+[
+  %{description: "Air Jordan 11", price: 110.0, closing_date: "2019-01-11"},
+  %{description: "Nike Air Force 1", price: 90.0, closing_date: "2019-01-13"},
+  %{description: "Air Jordan Why Not 2.0", price: 120.0, closing_date: "2019-01-20"}
+ ]
+ |> Enum.each(fn item -> Repo.insert!(Item.changeset(%Item{}, %{description: item.description, price: item.price, closing_date: Date.from_iso8601!(item.closing_date) })) end)
