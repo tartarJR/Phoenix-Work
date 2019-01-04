541b2c9e2f841aa73b399de6c9c241d7bc3ac222
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-04 04:42:23 +0200

    implemented app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,21 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 
-new Vue({
-  el: '#takso-app',
-  data: {
-    message: 'Hello there!'
-  }
+import "axios";
+import item from "./item";
+import bid from "./bid";
+
+Vue.use(VueRouter);
+
+Vue.component("item", item);
+Vue.component("bid", bid);
+
+var router = new VueRouter({
+  routes: [
+      { path: '/', component: item },
+      { path: '/items/:item_id', component: bid, name: "bid" },
+      { path: '*', redirect: '/' }
+  ]
 });
+
+new Vue({router}).$mount("#app");
